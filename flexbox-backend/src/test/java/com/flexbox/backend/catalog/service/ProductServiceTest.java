e97e52b24caf340d55050cdb4bf8fdd60bf6a675
Miranda Murphy <murp0528@algonquinlive.com>
2026-08-02 21:09:14 -0300

    Add seed data for product catalog API and create flyway migration script to populate data

---
+++ flexbox-backend/src/test/java/com/flexbox/backend/catalog/service/ProductServiceTest.java
@@ -0,0 +1,45 @@
+package com.flexbox.backend.catalog.service;
+
+import com.flexbox.backend.catalog.repository.ProductRepository;
+import com.flexbox.backend.catalog.response.ProductListResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @InjectMocks
+    ProductService productService;
+
+    @Mock
+    ProductRepository repo;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void getAllProducts_returnsEmptyList_whenNoProducts() {
+        when(repo.findAll()).thenReturn(List.of());
+
+        ProductListResponse response = productService.getAllProducts();
+
+        assertThat(response.products()).isEmpty();
+    }
+
+    @Test
+    void getProductById() {
+    }
+}
