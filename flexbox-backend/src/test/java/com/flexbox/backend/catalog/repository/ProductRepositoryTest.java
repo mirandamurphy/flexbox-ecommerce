34598bfdc0aae2dd47fd27009855ef6b83026b25
Miranda Murphy <murp0528@algonquinlive.com>
2026-08-04 10:58:45 -0300

    Add tests for custom query in SubscriptionBoxPriceRepository

--- flexbox-backend/src/test/java/com/flexbox/backend/catalog/repository/ProductRepositoryTest.java
+++ flexbox-backend/src/test/java/com/flexbox/backend/catalog/repository/ProductRepositoryTest.java
@@ -1,18 +1,12 @@
 package com.flexbox.backend.catalog.repository;
 
 import com.flexbox.backend.TestcontainersConfiguration;
-import com.flexbox.backend.catalog.model.Category;
-import com.flexbox.backend.catalog.model.Product;
 import org.hibernate.Hibernate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,14 +16,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ProductRepositoryTest {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductRepository repo;
 
     // Tests that @EntityGraph is working
     @Test
     void findById_shouldEagerlyFetchCategory() {
         long id = 9;
 
-        var product = productRepository.findById(id).orElseThrow();
+        var product = repo.findById(id).orElseThrow();
 
         assertThat(Hibernate.isInitialized(product.getCategory())).isTrue();
     }
